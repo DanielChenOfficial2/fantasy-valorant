2345b5118d96cd6a124eec8852f933d65879309f
daniel <daniel@spacetime-eng.com>
2024-04-17 22:40:29 +0900

    #6 Display user email on login

--- website/frontend/js/login.js
+++ website/frontend/js/login.js
@@ -22,21 +22,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyDiZli11fN1qlg8qoJvntycALm9eyS9Iwk",
-
   authDomain: "fantasy-valorant-d2588.firebaseapp.com",
-
   projectId: "fantasy-valorant-d2588",
-
   storageBucket: "fantasy-valorant-d2588.appspot.com",
-
   messagingSenderId: "419164409115",
-
   appId: "1:419164409115:web:3172a1aa23ccc3c99317be",
-
   measurementId: "G-GK0SLGNZ1E"
-
 };
 
 ;
@@ -52,26 +44,20 @@ window.addEventListener('load', () => {
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
-    signInSuccessUrl: '../html/success.html',
-    // emailLinkSignIn: function() {
-    //   return {
-    //     url: '../html/success.html',
-    //   }
-    // },
-
-    // Other config options...
   });
 
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is already signed in, redirect to appropriate page
       console.log('User already signed in:', user);
-      window.location.href = '../html/success.html';
-      // Your redirection logic here (e.g., welcome page)
+      const username = user.email || user.displayName; // Use email or display name if available
+      const redirectUrl = "../html/success.html"; // Replace with your desired target page
+
+      window.location.href = `${redirectUrl}?username=${username}`;
     } else {
       // User is not signed in, proceed with FirebaseUI login flow
       const uiConfig = {
-          signInSuccessUrl: '/success.html',
+          // signInSuccessUrl: '../html/success.html',
           signInOptions: [
               firebase.auth.EmailAuthProvider.PROVIDER_ID
           ]
@@ -79,25 +65,4 @@ window.addEventListener('load', () => {
       // Start FirebaseUI with the configuration
     }
   });
-  
-
-  const loginForm = document.getElementById('loginForm');
-  loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const loginUsername = document.getElementById('loginUsername');
-    console.log(loginUsername.value);
-
-    const loginPassword = document.getElementById('loginPassword');
-    console.log(loginPassword.value)
-  })
-  
-  const registerForm = document.getElementById('registerForm');
-  registerForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const registerUsername = document.getElementById('registerUsername');
-    console.log(registerUsername.value);
-
-    const registerPassword = document.getElementById('registerPassword');
-    console.log(registerPassword.value)
-  })
 });
